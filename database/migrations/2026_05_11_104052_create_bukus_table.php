83fc094ce07272e6a96b4766c5181f1a80c313f7
rahmihamka356-wq <rahmihamka356@gmail.com>
2026-05-11 19:31:21 +0800

    Berhasil menambahkan folder capture dan hasil akhir

---
+++ database/migrations/2026_05_11_104052_create_bukus_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode_buku')->unique();
+            $table->string('judul');
+            $table->string('kategori');
+            $table->string('penulis');
+            $table->integer('stok');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
